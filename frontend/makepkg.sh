95d5f34efd5e26ad55c5912ec25ed4c116fad80d
Edmunt Pienkowsky <roed@onet.eu>
2020-09-08 09:50:02 +0200

    Update makepkg.sh scripts

--- frontend/makepkg.sh
+++ frontend/makepkg.sh
@@ -1,8 +1,19 @@
 #!/bin/bash -e
 
-# PKG_STAMP=$(git show -s --format=%ct)
-PKG_STAMP=1598344037
-PKG_DATE=$(date -u --date=@${PKG_STAMP})
-echo "Package timestamp: ${PKG_DATE} (${PKG_STAMP})"
+GIT_PATHSPEC=:/frontend
+
+if [ -n "$(git status --porcelain ${GIT_PATHSPEC})" ]; then 
+	echo 'There are uncommited changes'
+	exit 1
+fi
+
+PKG_STAMP=$(git log -n 1 '--pretty=format:%ct' -- ${GIT_PATHSPEC})
+if [ -z "${PKG_STAMP}" ]; then
+	echo 'Unable to get last commit timestamp'
+	exit 1
+fi
+
+PKG_DATE=$(date -u --date=@${PKG_STAMP} '+%F %T')
+echo "Package timestamp: ${PKG_DATE} [@${PKG_STAMP}]"
 
 /usr/bin/makepkg "$@" SOURCE_DATE_EPOCH=${PKG_STAMP}
